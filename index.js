9ee5fbfa9296dd3926353fc233a4e13eb09ea52a
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-10-22 12:58:15 +0530

    reading and writing filesysytem in javascript

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
-
+//reading from the text
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
+const textOut =
+  "this is  we know about node js:  ${textIn}.\nCreated on ${Date.now()}";
+
+//writing in new module
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("file written");
