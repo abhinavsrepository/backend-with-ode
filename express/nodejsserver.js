e58ff747dbef77d7a1de0bb38588544870bfa2ff
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-06 20:05:20 +0530

    made the server

---
+++ express/nodejsserver.js
@@ -0,0 +1,24 @@
+const { checkPrime } = require('crypto');
+const http =require('http');
+// the http module has a createserver method 
+// take 1 arg:
+//callback has 2 args :req,res
+const server = http.createServer((req,res)=>{
+console.log (req)
+// res ==our way torespond the msgs
+// http messages 
+// 1. start-line -checkPrime
+// 2. Headers
+// 3.body
+//  write Head takes 2 args :
+// status code 
+// object for the mime-type
+res.writeHead(200,{'content-type':'text/html'});
+
+res.write ('<h1>helllo to the world becoming best coder in the world </h1>')
+res.end();
+});
+// createServer returns an object  with a listen method 
+// listen takes 1 arg 
+// port to for http traffic on 
+server.listen(3000);
